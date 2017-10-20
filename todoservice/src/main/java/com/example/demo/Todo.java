35688ec4f3e8f38fac32e43db5d88180a47457d4
getmubarak <getmub@gmail.com>
2017-10-20 18:44:29 +0530

    first commit

---
+++ todoservice/src/main/java/com/example/demo/Todo.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection="todo")
+public class Todo {
+
+    @Id
+    public String id;
+    public String title;
+
+    public Todo() {}
+
+    public void setTitle(String title){
+    	this.title = title;
+    }
+    public String getTitle(){
+    	return title;
+    }
+}
---
+++ todoservice/src/main/java/com/example/demo/TodoController.java
@@ -0,0 +1,27 @@
+package com.example.demo;
+
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/todo")
+public class TodoController {
+
+	 @Autowired
+	 public TodoRepository todoRepository;
+	 
+	 @RequestMapping("/create")
+	 public String create(Todo todo) {
+	  todo = todoRepository.save(todo);
+	  return "Todo created successfully";
+	 }
+	 
+	@RequestMapping("/all")
+	 public List<Todo> readAll() {
+	  return todoRepository.findAll();
+	}
+}
