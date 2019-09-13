07ba459d171a02cf43454f34fcd61d2d4196057d
getmubarak <getmub@gmail.com>
2019-09-13 14:26:25 +0530

    Create p20.java

---
+++ p20.java
@@ -0,0 +1,39 @@
+class Account
+{
+     public abstract withdraw(double amount);
+     public abstract deposit(double amount);
+     
+     public void Save();
+}
+
+class SA extends Account
+{
+    public void Save()
+    {
+      //logic
+    }
+     public withdraw(double amount)
+     {
+        //logic
+     }
+     public deposit(double amount)
+     {
+        //logic
+     }
+}
+
+class CA extends Account
+{
+    public void Save()
+    {
+      //logic
+    }
+     public withdraw(double amount)
+     {
+        //logic
+     }
+     public deposit(double amount)
+     {
+        //logic
+     }
+}
