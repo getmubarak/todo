4f2f3e1e1e5c1d42fba893c341b61eb4e07f1f2f
getmubarak <getmub@gmail.com>
2019-09-13 14:56:30 +0530

    Create p3.java

---
+++ p3.java
@@ -0,0 +1,53 @@
+package problem3;
+interface Factory
+{
+	 Connection getCon();
+	 Command getCmd();
+	 Transaction getTran();
+}
+class SqlFactory implements Factory
+{
+	public Connection getCon(){
+		return new SqlConnection();
+	}
+	public Command getCmd(){
+		return new SqlCommand();
+	}
+	public Transaction getTran(){
+		return new SqlTransaction();
+	}
+}
+class OraFactory implemnts Factory
+{
+	public Connection getCon(){
+		return new OraConnection();
+	}
+	public Command getCmd(){
+		return new OraCommand();
+	}
+	public Transaction getTran(){
+		return new OraTransaction();
+	}
+}
+public class Entry {
+	public static void do(Factory f)
+	{
+		Connection connection = f.getCon();
+		connection.open("mydb;scott;tiger");
+		Transaction transaction = f.getTran();
+		transaction.begin(connection);
+		
+		Command cmd1 = f.getCmd();
+		cmd1.execute(transaction, "insert into emp values(10,'jack',2500')");
+
+		Command cmd2 = f.getCmd();
+		cmd2.execute(transaction, "insert into emp values(20,'jill',4300')");
+		
+		transaction.commit();
+		connection.close();
+	}
+	public static void main() {
+		
+		
+	}
+}
